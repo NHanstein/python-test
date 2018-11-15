3bdf51f9f3861ee5b391bd738227b4b9d6c77dbd
Nikolas Hanstein <nikolas.hanstein@gmail.com>
2018-11-15 10:56:13 +0100

    Moved tests into add.py

--- add.py
+++ add.py
@@ -1,2 +1,14 @@
+from random import randint
+
 def add(a, b):
     return a + b
+
+def test_commut():
+    a = randint(1, 10)
+    b = randint(1, 10)
+    assert add(a, b) == add(b, a)
+
+def test_spec():
+    a = randint(1, 10)
+    b = randint(1, 10)
+    assert add(a, b) == a + b
